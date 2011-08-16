0fccfdf64fcd6c27621c0018fd9aca0bf422ed72
Vineet <naikvin@gmail.com>
2011-08-16 23:56:18 +0530

    add exercises for chapter 6 (Sequences)

---
+++ exercises/sequences.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+
+import string
+import keyword
+
+# 6.3 a
+def sort_numlist_desc(li):
+    "Enter a list of numbers and sort the values in largest-to-smallest order."
+    li.sort(reverse=True)
+    return li
+
+# 6.3 b
+def sort_string_desc(s):
+    """
+    Do the same thing, but for strings and in reverse alphabetical (largest-to-
+    smallest lexicographic) order.
+    """
+    return s[::-1]
+
+# 6.2
+def idcheck(identifier):
+    """
+    String Identifiers. Modify the idcheck.py script in Example 6-1 such that it will
+    determine the validity of identifiers of length 1 as well as be able to detect if an
+    identifier is a keyword. For the latter part of the exercise, you may use the keyword
+    module (specifically the keyword.kwlist list) to aid in your cause.
+    """
+    alphas = string.letters + '_'
+    nums = string.digits
+    if len(identifier) > 0:
+        if keyword.iskeyword(identifier):
+            return False
+        elif identifier[0] not in alphas:
+            return False
+        elif len(identifier) == 1:
+            return True
+        else:            
+            for otherChar in identifier[1:]:
+                if otherChar not in alphas + nums:
+                    return False
+                else:
+                    return True
+
+# 6.5 b
+def string_cmp(string1, string2):
+    """
+    Determine if two strings match (without using comparison operators or the cmp
+    () built-in function) by scanning each string. Extra credit: Add case-
+    insensitivity to your solution.
+    """
+    string1 = string1.lower()
+    string2 = string2.lower()
+    for i, s in enumerate(string1):
+        if string2[i] != s:
+            return False
+    return True
+            
+    
+
+
+
+
---
+++ tests/test_sequences.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import unittest
+import exercises.sequences
+
+class TestSequencesExercise(unittest.TestCase):
+
+    def test_sort_numlist_desc(self):
+        self.assertEqual([34, 10, 5, 3, 1, 0], exercises.sequences.sort_numlist_desc([10, 3, 5, 0, 1, 34]))
+
+    def test_sort_string_desc(self):
+        self.assertEqual('dlrowolleh', exercises.sequences.sort_string_desc('helloworld'))
+
+    def test_idcheck(self):
+        self.assertTrue(exercises.sequences.idcheck('abc'))
+        self.assertTrue(exercises.sequences.idcheck('a'))
+        self.assertFalse(exercises.sequences.idcheck('3cf'))
+        self.assertFalse(exercises.sequences.idcheck('while'))
+        self.assertTrue(exercises.sequences.idcheck('True')) # True is not a keyword. WTF!!
+
+    def test_string_cmp(self):
+        self.assertTrue(exercises.sequences.string_cmp('hello4', 'hello4'))
+        self.assertTrue(exercises.sequences.string_cmp('55WorlD', '55woRLd'))
+
+
+        
+        
